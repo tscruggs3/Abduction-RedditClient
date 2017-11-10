5bf50f77abf57bbdf146d77e56609d4601a66cd6
tscruggs3 <scruggst@carleton.edu>
2017-11-09 21:52:13 -0600

    Made buttons do simple actions.

--- browserforreddit/src/SubredditGUI.java
+++ browserforreddit/src/SubredditGUI.java
@@ -9,8 +9,10 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import java.util.List;
 
 
@@ -21,9 +23,9 @@ import java.util.List;
  */
 
 public class SubredditGUI {
-    private static final double SCENE_WIDTH = 1100;
-    private static final double SCENE_HEIGHT = 750;
-    private static final double MIN_UPVOTE_WIDTH = 90;
+    private static final double SCENE_WIDTH = 600;
+    private static final double SCENE_HEIGHT = 400;
+    private static final double MIN_UPVOTE_WIDTH = 110;
     private static final double MIN_TITLE_WIDTH = 250;
     private static final int POSTS_PER_PAGE = 10;
 
@@ -50,6 +52,7 @@ public class SubredditGUI {
         titlePane.setAlignment(Pos.TOP_LEFT);
 
         Text heading = new Text(title);
+        heading.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
         titlePane.getChildren().add(heading);
 
         return titlePane;
@@ -61,7 +64,7 @@ public class SubredditGUI {
         pane.setAlignment(Pos.TOP_LEFT);
 
         MenuBar menuBar = new MenuBar();
-        menuBar.setMinWidth(SCENE_WIDTH+1000);
+        menuBar.setMinWidth(SCENE_WIDTH);
 
         Menu refreshMenu = new Menu("Refresh");
 
@@ -125,33 +128,64 @@ public class SubredditGUI {
         postPane.setVgap(0);
         postPane.setHgap(5);
 
-        Button upvote = new Button("upvote");
+        /* TODO: I think this is the something close to the code for making the images buttons.
+
+        Image backImage = new Image(getClass().getResourceAsStream("back.png"));
+        Button upvote = new Button("", new ImageView(backImage));
+         */
+        Button upvote = new Button("Upvote");
         upvote.setMinWidth(MIN_UPVOTE_WIDTH);
         postPane.add(upvote, 1, 0);
 
         Text postNumb = new Text(Integer.toString(postNumber));
         postPane.add(postNumb, 0, 1);
 
+
         Text voteCount = new Text("         " + Integer.toString(postPreview.getVote()));
         postPane.add(voteCount, 1, 1);
 
+        upvote.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+               upvote.setText("Upvoted!");
+               upvote.setTextFill(Color.GREEN);
+               //TODO: tell controller to increase vote by 1
+                voteCount.setText("         " + Integer.toString(postPreview.getVote()));
+            }
+        });
+
+
         Button postContent = new Button(postPreview.getTitle());
         postContent.setMinWidth(MIN_TITLE_WIDTH);
+        postContent.setOnAction(evt -> postContent.setText("Clicked on " + postPreview.getContentURL()));
         postPane.add(postContent, 2, 1);
 
         String userEntry = "By " + postPreview.getUsername();
-        Text username = new Text(userEntry);
+        Button username = new Button(userEntry);
+        username.setMinWidth(MIN_TITLE_WIDTH);
+        username.setOnAction(evt -> username.setText("Clicked on " + postPreview.getUsername()));
         postPane.add(username, 3, 1);
 
         Button downvote = new Button("downvote");
         downvote.setMinWidth(MIN_UPVOTE_WIDTH);
         postPane.add(downvote, 1, 2);
 
+        downvote.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                downvote.setText("Downvoted!");
+                downvote.setTextFill(Color.RED);
+                //TODO: tell controller to decrease vote by 1
+                voteCount.setText("         " + Integer.toString(postPreview.getVote()));
+            }
+        });
+
         Button comments = new Button("Comments");
         comments.setMinWidth(MIN_UPVOTE_WIDTH);
         /* TODO on click, the comments button should go to the comments page
 
          */
+        comments.setOnAction(evt -> comments.setText("Clicked on " + postPreview.getCommentURL()));
         postPane.add(comments, 3, 2);
 
         return postPane;
